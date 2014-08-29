4420acf60a9f873591ba8ad0d9fab50a4f21e6cf
Ryota Arai <ryota.arai@gmail.com>
2014-08-29 18:02:04 +0900

    Make default behavior the same as aws-cli.

--- ec2-describe-instances-go.go
+++ ec2-describe-instances-go.go
@@ -12,7 +12,9 @@ import (
 
 func main() {
 	var regionStr string
+	var flatten bool
 	flag.StringVar(&regionStr, "r", "us-east-1", "Region")
+	flag.BoolVar(&flatten, "f", false, "Flatten instances (The result will be an array)")
 	flag.Parse()
 
 	region, ok := aws.Regions[regionStr]
@@ -32,12 +34,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	instances := []ec2.Instance{}
-	for _, reservation := range resp.Reservations {
-		instances = append(instances, reservation.Instances...)
+	var obj interface{}
+
+	if flatten {
+		instances := []ec2.Instance{}
+		for _, reservation := range resp.Reservations {
+			instances = append(instances, reservation.Instances...)
+		}
+		obj = instances
+	} else {
+		obj = resp
 	}
 
-	b, err := json.Marshal(instances)
+	b, err := json.Marshal(obj)
 	if err != nil {
 		log.Fatal(err)
 	}
