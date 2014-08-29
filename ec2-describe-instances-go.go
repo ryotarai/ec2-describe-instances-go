b46c9b11f3904355644a513b0a77714269c85440
Ryota Arai <ryota.arai@gmail.com>
2014-08-29 12:05:37 +0900

    Understand -r option as region.

--- ec2-describe-instances-go.go
+++ ec2-describe-instances-go.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"log"
 
@@ -10,12 +12,21 @@ import (
 )
 
 func main() {
+	var regionStr string
+	flag.StringVar(&regionStr, "r", "us-east-1", "Region")
+	flag.Parse()
+
+	region := strToRegion(regionStr)
+	if region == nil {
+		log.Fatal(errors.New("Region " + regionStr + " is unknown."))
+	}
+
 	auth, err := aws.EnvAuth()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	client := ec2.New(auth, aws.APNortheast)
+	client := ec2.New(auth, *region)
 
 	resp, err := client.Instances(nil, nil)
 	if err != nil {
@@ -33,4 +44,27 @@ func main() {
 	}
 
 	fmt.Printf("%s\n", b)
+}
+
+func strToRegion(str string) *aws.Region {
+	switch str {
+	case "us-east-1":
+		return &aws.USEast
+	case "us-west-1":
+		return &aws.USWest
+	case "us-west-2":
+		return &aws.USWest2
+	case "eu-west-1":
+		return &aws.EUWest
+	case "ap-southeast-1":
+		return &aws.APSoutheast
+	case "ap-southeast-2":
+		return &aws.APSoutheast2
+	case "ap-northeast-1":
+		return &aws.APNortheast
+	case "sa-east-1":
+		return &aws.SAEast
+	}
+
+	return nil
 }
